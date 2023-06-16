11d0cbc171a46d73a18cf1f135e83f1ec0ac83ce
Federico Dolce <psykopear@gmail.com>
2023-06-16 17:24:05 +0200

    Slightly better plot

--- scripts/plot.py
+++ scripts/plot.py
@@ -1,5 +1,4 @@
 import csv
-import glob
 import argparse
 
 from matplotlib import pyplot as plt
@@ -35,7 +34,7 @@ colors = ['red', 'blue', 'green', 'orange', 'purple', 'black']
 for i, file_data in enumerate(data):
     elapsed = file_data['elapsed']
     lag = file_data['lag']
-    ax.plot(elapsed, lag, color=colors[i], label=file_data['name'])
+    ax.plot(elapsed, lag, color=colors[i], label=file_data['name'], linewidth=0.5)
 
 # Set labels and title for the plot
 ax.set_xlabel('Time (s)')
@@ -47,6 +46,6 @@ ax.legend()
 
 # Display or save the plot
 if args.output:
-    plt.savefig(args.output, dpi=199)
+    plt.savefig(args.output, dpi=200)
 else:
     plt.show()
